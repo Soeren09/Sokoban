c5c7acc75f30416f1fc41731a0b3864a18045593
Soeren <soerengraabaek@hotmail.com>
2019-10-21 11:00:57 +0200

    Updated loader

--- Loader.py
+++ Loader.py
@@ -1,5 +1,23 @@
+import io    
 
 
-class loader:
-    def __init__(self, path)
-        
+
+class CreateMap:
+    def __init__(self, name):
+        self.name = name
+
+        with open(self.name, 'r') as file:
+            for line in file:   
+                if (line[0] == 'X'):
+                    size = len(line)
+
+                    for char in line:
+                        print(char)
+
+                    print(line[0], size)
+
+
+
+
+p = CreateMap("/home/soeren/Git/Sokoban/2018-competation-map_2018.txt")
+
