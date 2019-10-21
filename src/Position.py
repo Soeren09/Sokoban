d812ca3bef2ecaa132e7ec0ed045d0977e41ba9d
Emil Ancker <emil@ancker.com>
2019-10-21 10:49:35 +0200

    Position class

---
+++ src/Position.py
@@ -0,0 +1,17 @@
+class Position:
+    MAX_COL = 1000 # Shared value
+    # Init Position with row and column number
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+    # hashCode gives a unique number for each 2D position
+    def hashCode(self):
+        return self.MAX_COL*self.row + self.col
+    # Check if two positions are equal
+    def equal(self, pos):
+        if ( pos is None ):
+            return False
+        if ( pos.hashCode() == self.hashCode() ):
+            if ( pos.row == self.row and pos.col == self.col ):
+                return True
+        return False
